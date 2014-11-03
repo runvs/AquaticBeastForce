dd0692f1a18d0ee865b0a94b298a4b0830c00fc2
Thunraz <julian.dinges@gmail.com>
2014-11-03 23:05:25 +0100

    Player moves and rotates

--- source/Player.hx
+++ source/Player.hx
@@ -2,6 +2,7 @@ package ;
 import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.input.keyboard.FlxKey;
 import flixel.util.FlxVector;
 import flixel.util.FlxColor;
 
@@ -12,11 +13,16 @@ import flixel.util.FlxColor;
 class Player extends FlxBasic
 {
     public var position:FlxVector;
+    public var rotation:Float;
+    
     private var _sprite:FlxSprite;
+    private var _circle:Float;
     
 	public function new(x:Float, y:Float)
 	{
 		position = new FlxVector(x, y);
+        
+        _circle = Math.PI * 2;
         
         _sprite = new FlxSprite(position.x, position.y);
         _sprite.loadGraphic(AssetPaths.player__png, true, 16, 16);
@@ -31,6 +37,7 @@ class Player extends FlxBasic
     {
         getInput();
         _sprite.setPosition(position.x, position.y);
+        _sprite.angle = rotation * 180/Math.PI;
         _sprite.update();
         
         super.update();
@@ -44,6 +51,36 @@ class Player extends FlxBasic
     
     private function getInput():Void
     {
+        var up:Bool = FlxG.keys.anyPressed(["W", "UP"]);
+        var down:Bool = FlxG.keys.anyPressed(["S", "DOWN"]);
+        var left:Bool = FlxG.keys.anyPressed(["A", "LEFT"]);
+        var right:Bool = FlxG.keys.anyPressed(["D", "RIGHT"]);
         
+        if (left)
+        {
+            rotation = (rotation - FlxG.elapsed * 5) % _circle;
+        }
+        else if (right)
+        {
+            rotation = (rotation + FlxG.elapsed * 5) % _circle;
+        }
+        
+        if (up)
+        {
+            move(40 * FlxG.elapsed);
+        }
+        else if (down)
+        {
+            move(-40 * FlxG.elapsed);
+        }
+    }
+    
+    private function move(distance:Float):Void
+    {
+        var dx:Float = Math.cos(rotation + Math.PI * 3/2) * distance;
+        var dy:Float = Math.sin(rotation + Math.PI * 3/2) * distance;
+        
+        position.x += dx;
+        position.y += dy;
     }
 }
