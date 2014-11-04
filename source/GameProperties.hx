1836f2bf22eaa9474accfa4ffb0af456114d0f03
Thunraz <julian.dinges@gmail.com>
2014-11-04 21:45:52 +0100

    Reduced player rotation speed

--- source/GameProperties.hx
+++ source/GameProperties.hx
@@ -7,5 +7,5 @@ package ;
 class GameProperties
 {
     public static var PlayerMovementSpeed:Float = 40;
-    public static var PlayerRotationSpeed:Float = 4;
+    public static var PlayerRotationSpeed:Float = 2;
 }
