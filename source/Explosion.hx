e6a76a6888acb94fd7c3213eb73126e1b26b73b5
Thunraz <julian.dinges@gmail.com>
2015-04-12 02:40:49 +0200

    Renamed explo1.png to explosionBig.png

--- source/Explosion.hx
+++ source/Explosion.hx
@@ -29,7 +29,7 @@ class Explosion extends FlxSprite
         }
         else
         {
-            loadGraphic(AssetPaths.explo1__png, true, 16, 16);
+            loadGraphic(AssetPaths.explosionBig__png, true, 16, 16);
         }
         
 		animation.add("explode", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 20, false);
